63a04b38c22ed839e70ce149fea52cfec999d94f
firemanphil <fireman.phil@gmail.com>
2017-05-24 08:49:51 +0100

    Upping version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name = 'ecs_explorer',
     packages = ['ecs_explorer'], # this must be the same as the name above
-    version = '0.4',
+    version = '0.5',
     description = 'CLI for exploring AWS ECS resources',
     author = 'Philip Ince',
     author_email = 'fireman.phil@gmail.com',
     url = 'https://github.com/firemanphil/ecs_explorer', # use the URL to the github repo
-    download_url = 'https://github.com/firemanphil/ecs_explorer/archive/0.4.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/firemanphil/ecs_explorer/archive/0.5.tar.gz', # I'll explain this in a second
     keywords = ['AWS', 'ECS', 'urwid', 'ncurses', 'cli'], # arbitrary keywords
     install_requires = ['boto3>=1.4.4', 'urwid>=1.3.1'],
     classifiers = [],
